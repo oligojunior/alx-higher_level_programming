b3a758582e9f50c340550ebd6bc2cddf2229aa61
oligojunior <rkudofia@gmail.com>
2023-01-24 16:39:21 +0000

    task completed

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """An empty class called Square that defines a square"""
 
+
 class Square:
-    """An empty class Square that defines a square.
-    """
+    """An empty class Square that defines a square."""
     pass
