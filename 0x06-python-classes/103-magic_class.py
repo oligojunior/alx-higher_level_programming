d4dcd4ea2702e900a744724e5abeb37d655311c5
oligojunior <rkudofia@gmail.com>
2023-01-24 17:12:37 +0000

    advanced tasks  editted

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-""" Module providing class 'MagicClass' to replicate bytecode for ALX """
+""" Class that stores the properties of a circumference """
 import math
 
 
-class MagicClass():
-    """ Definition of a class to replicate the afformentioned bytecode """
+class MagicClass:
+
+    """Class that stores the properties
+    of a circumference"""
     def __init__(self, radius=0):
-        """ Instantiate a MagicClass object to represent a circle """
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
         self.__radius = radius
 
+    """ Method that calculates the area of the circumference """
     def area(self):
-        """ Compute the area of a circle """
-        return (self.__radius ** 2) * math.pi
+        return ((self.__radius ** 2) * math.pi)
 
+    """ Method that calculates the perimeter of a circumference """
     def circumference(self):
-        """ Compute the circumference of a circle """
-        return 2 * math.pi * self.__radius
+        return (2 * math.pi * self.__radius)
 
